66827f7a904a3a2369318a287045cde3c53d4f48
Remigiusz <repogplus@gmail.com>
2022-04-01 20:15:49 +0200

    Stage completed

--- Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/task/src/processor/Main.java
@@ -1,6 +1,5 @@
 package processor;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,22 +10,8 @@ public class Main {
 
         int[][] matrixA = new int[aRows][aColumns];
         fillMatrixA(matrixA);
-
-        int bRows = scanner.nextInt();
-        int bColumns = scanner.nextInt();
-
-        int[][] matrixB = new int[bRows][bColumns];
-        fillMatrixB(matrixB);
-
-        if (isMatrixValid(aRows, aColumns, bRows, bColumns)) {
-            addMatrices(matrixA, matrixB);
-        } else {
-            System.out.println("ERROR");
-        }
-    }
-
-    public static boolean isMatrixValid(int aRows, int aColumns, int bRows, int bColumns) {
-        return aRows == bRows && aColumns == bColumns;
+        int constant = scanner.nextInt();
+        multiplyMatrix(matrixA, constant);
     }
 
     public static void fillMatrixA(int[][] matrixA) {
@@ -40,21 +25,10 @@ public class Main {
         }
     }
 
-    public static void fillMatrixB(int[][] matrixB) {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < matrixB.length; i++) {
-            for (int j = 0; j < matrixB[0].length; j++) {
-                matrixB[i][j] = scanner.nextInt();
-//                System.out.print(matrixB[i][j]);
-            }
-//            System.out.println();
-        }
-    }
-
-    public static void addMatrices(int[][] matrixA, int[][] matrixB) {
-        for (int i = 0; i < matrixA.length; i++) {
+    public static void multiplyMatrix(int[][] matrixA, int constant) {
+        for (int[] ints : matrixA) {
             for (int j = 0; j < matrixA[0].length; j++) {
-                System.out.print(matrixA[i][j] + matrixB[i][j] + " ");
+                System.out.print(ints[j] * constant + " ");
             }
             System.out.println();
         }
