23138c3f9a6d84565372da36181255d2ce33835f
Remigiusz <repogplus@gmail.com>
2022-04-04 21:26:20 +0200

    Stage completed

--- Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/task/src/processor/Main.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         boolean exit = false;
 
         while (!exit) {
-            System.out.println("1. Add matrices");
-            System.out.println("2. Multiply matrix by a constant");
-            System.out.println("3. Multiply matrices");
-            System.out.println("0. Exit");
+            System.out.println("1. Add matrices\n" +
+                    "2. Multiply matrix by a constant\n" +
+                    "3. Multiply matrices\n" +
+                    "4. Transpose matrix\n" +
+                    "0. Exit");
             Scanner scanner = new Scanner(System.in);
             int choice = scanner.nextInt();
             switch (choice) {
+                case 0:
+                    exit = true;
+                    break;
                 case 1:
                     double[][] matrixA = fillMatrixA();
                     double[][] matrixB = fillMatrixB();
@@ -41,8 +43,8 @@ public class Main {
                         return;
                     }
                     break;
-                case 0:
-                    exit = true;
+                case 4:
+                    selectTransposeMode();
                     break;
                 default:
                     System.out.println("Please choose correct function.");
@@ -58,6 +60,36 @@ public class Main {
 
     public static boolean isMatrixValid(double[][] matrixA, double[][] matrixB) {
         return matrixA[0].length == matrixB.length;
+    }
+
+    public static void selectTransposeMode() {
+
+        System.out.println(" 1. Main diagonal\n" +
+                "2. Side diagonal\n" +
+                "3. Vertical line\n" +
+                "4. Horizontal line");
+        Scanner scanner = new Scanner(System.in);
+        int mode = scanner.nextInt();
+        System.out.println("The result is: ");
+        switch (mode) {
+
+            case 1:
+                double[][] matrixA = fillMatrixA();
+                transposeMainDiagonal(matrixA);
+                break;
+            case 2:
+                matrixA = fillMatrixA();
+                transposeSideDiagonal(matrixA);
+                break;
+            case 3:
+                matrixA = fillMatrixA();
+                transposeVertical(matrixA);
+                break;
+            case 4:
+                matrixA = fillMatrixA();
+                transposeHorizontal(matrixA);
+                break;
+        }
     }
 
     public static double[][] fillMatrixA() {
@@ -100,9 +132,9 @@ public class Main {
     }
 
     public static void multiplyMatrix(double[][] matrixA, int constant) {
-        for (double[] ints : matrixA) {
+        for (double[] doubles : matrixA) {
             for (int j = 0; j < matrixA[0].length; j++) {
-                System.out.print(ints[j] * constant + " ");
+                System.out.print(doubles[j] * constant + " ");
             }
             System.out.println();
         }
@@ -116,12 +148,9 @@ public class Main {
             int nextColumn = 0;                     // musi być długość mtrycy B dla warunku mnożenia tablic
             while (nextColumn < matrixB[0].length) { // sprawdza wszystie rzędy w drugiej matrycy (tu columnu
                 // zanim przejdzie do nowego rzędu w drugiej matrycy
-//                System.out.println("\n nextcolumn: "+ nextColumn);
-//                System.out.println(" i: "+ i);
                 for (int j = 0; j < matrixA[0].length; j++) {
-//                System.out.println(" j: "+ j);
+
                     multiplicationSum += matrixA[i][j] * matrixB[j][nextColumn];
-//                System.out.print((int)matrixA[i][j] + "*" + (int) matrixB[j][nextColumn] +"=" + (matrixA[i][j] * matrixB[j][nextColumn]));
                 }
                 System.out.print("  " + multiplicationSum + " ");
                 multiplicationSum = 0;
@@ -131,5 +160,41 @@ public class Main {
         }
 
     }
-}
 
+    public static void transposeMainDiagonal(double[][] matrixA) {  // axis from left upper corner
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[0].length; j++) {
+                System.out.println(matrixA[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void transposeSideDiagonal(double[][] matrixA) { // axis from right upper corner
+        for (int i = matrixA.length - 1; i >= 0; i--) {
+            for (int j = matrixA[0].length - 1; j >= 0; j--) {
+                System.out.println(matrixA[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void transposeVertical(double[][] matrixA) {
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = matrixA[0].length - 1; j >= 0; j--) {  // vertical axis 0,0 becomes 0,3 and vice versa
+                System.out.println(matrixA[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void transposeHorizontal(double[][] matrixA) {
+        for (int i = matrixA.length - 1; i >= 0; i--) {    // horizontal axis 0,0 becomes 3,0 and vice versa
+            for (int j = 0; j < matrixA[0].length; j++) {
+                System.out.println(matrixA[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
